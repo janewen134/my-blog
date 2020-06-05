42e6d6814cf37e07e59405c132c798fb19ff80af
Jing <jxw1070@student.bham.ac.uk>
2020-06-05 15:11:50 +0100

    dont know why not able to run

--- website_root/events/models.py
+++ website_root/events/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 from django.db import models
 
-class Event(models.Model):
-    name = models.CharField('Event Name', max_length=120)
-    event_date = models.DateTimeField('Event Date')
-    venue = models.CharField(max_length=120)
-    manager = models.CharField(max_length = 60)
-    description = models.TextField(blank=True)
+class Teacher(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField()
+    class Meta:
+        db_table = 'teacher'
+
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField('Name', max_length=50)
+    age = models.IntegerField()
+    intime = models.DateField()
+    gender = models.IntegerField(max_length = 60)
+    # teacher = models.ForeignKey(Teacher, related_name='tutor')
+    class Meta:
+        db_table = 'student'
--- website_root/events/views.py
+++ website_root/events/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader, Context
+from .models import Student
 
-# Create your views here.
+def student_list(request):
+    t = loader.get_template("student_list.html")
+    studentList = Student.objects.all()
+    # teacher = Student.teacher.name
+    c = Context({"studentlist: ": studentList})
+    return HttpResponse(t.render(c))
